fe4ad4f66598721fe4a891fb6f87ad73ee2c46a6
jenshuchi <jack115347@gmail.com>
2022-04-16 03:44:39 -0400

    feat: add dev mode

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
 import asyncio
 import json
 import os
+import random
 import sys
 
 import aiohttp
 
+IS_DEV = os.getenv("ENV") == "development"
+
+
 async def get_route(session, bus_number, direction):
-    # async with session.get(f"https://ptx.transportdata.tw/MOTC/v2/Bus/StopOfRoute/City/Taipei?%24filter=RouteName%2FZh_tw%20eq%20'{bus_number}'%20and%20Direction%20eq%20{direction}&%24top=30&%24format=JSON",
-    #         headers={
-    #         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36"
-    #         }
-    #     ) as response:
-
-    #         if response.status != 200:
-    #             print(f"Error: status={response.status}")
-
-    #         data = await response.json()
-    #         json.dumps(data, separators=(":", ","), indent=4)
-    #         return data[0]["Stops"]
+    if IS_DEV:
     with open("StopOfRoute_672.json", "r") as f:
         data = json.load(f)
     return data[0]["Stops"]
+
+    async with session.get(f"https://ptx.transportdata.tw/MOTC/v2/Bus/StopOfRoute/City/Taipei?%24filter=RouteName%2FZh_tw%20eq%20'{bus_number}'%20and%20Direction%20eq%20{direction}&%24top=30&%24format=JSON",
+            headers={
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36"
+            }
+        ) as response:
+
+            if response.status != 200:
+                print(f"Error: status={response.status}")
+
+            data = await response.json()
+            json.dumps(data, separators=(":", ","), indent=4)
+            return data[0]["Stops"]
+    
 
 
 def get_target_stop_sequence(target_stop_name, stops):
@@ -33,21 +40,22 @@ def get_target_stop_sequence(target_stop_name, stops):
 
 
 async def get_running_buses(session, bus_number, direction):
-    # async with session.get(f"https://ptx.transportdata.tw/MOTC/v2/Bus/RealTimeNearStop/City/Taipei?%24filter=RouteName%2FZh_tw%20eq%20'{bus_number}'%20and%20Direction%20eq%20{direction}&%24top=30&%24format=JSON",
-    #         headers={
-    #         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36"
-    #         }
-    #     ) as response:
-
-    #         if response.status != 200:
-    #             print(f"Error: status={response.status}")
-
-    #         return await response.json()
-    import random
+    if IS_DEV:
     i = random.randint(1, 4)
     with open(f"RealTimeNearStop_672-{i}.json", "r") as f:
         data = json.load(f)
     return data
+
+    async with session.get(f"https://ptx.transportdata.tw/MOTC/v2/Bus/RealTimeNearStop/City/Taipei?%24filter=RouteName%2FZh_tw%20eq%20'{bus_number}'%20and%20Direction%20eq%20{direction}&%24top=30&%24format=JSON",
+            headers={
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36"
+            }
+        ) as response:
+
+            if response.status != 200:
+                print(f"Error: status={response.status}")
+
+            return await response.json()
 
 
 def has_bus_near_target(target, buses):
